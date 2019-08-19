e29a34ab35ef6c4aacc352a3ec08113f04b835c7
Alex <5429285+megabobik@users.noreply.github.com>
2019-08-19 02:46:18 +0200

    remade test cases

---
+++ src/test/java/org/github/kaninhop/parser/xml/magnolia/MagnoliaXmlParserTest.java
@@ -0,0 +1,15 @@
+package org.github.kaninhop.parser.xml.magnolia;
+
+import org.junit.Test;
+
+public class MagnoliaXmlParserTest {
+
+    @Test
+    public void test_xml_parser() throws Exception {
+//        AbstractParser parser = new MagnoliaXmlParser("/magnolia/fuzzylop-mgnl-content.xml");
+//        DataModel dataModel = parser.getModel();
+
+//        assertEquals("2019", dataModel.getWorkspaces().get(0).getNodes().get(0).getNodes().get(0).getName());
+//        assertEquals(4, dataModel.getWorkspaces().get(0).getNodes().get(0).getNodes().get(0).getNodes().get(0).getNodes().size());
+    }
+}
---
+++ src/test/java/org/github/kaninhop/parser/yaml/magnolia/MagnoliaYamlParserTest.java
@@ -0,0 +1,14 @@
+package org.github.kaninhop.parser.yaml.magnolia;
+
+import org.junit.Test;
+
+public class MagnoliaYamlParserTest {
+
+    @Test
+    public void test_parser(){
+//        AbstractParser parser = new MagnoliaYamlParser("/magnolia/fuzzylop-mgnl-content.yaml");
+//        DataModel dataModel = parser.getModel();
+
+//        assertEquals("",  dataModel.getWorkspaces().get(0).getWorkspaceName());
+    }
+}
